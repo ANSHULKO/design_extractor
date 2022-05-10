c2ccf91c8c1cd2111c1333f8185cd66cf2f4d247
ANSHUMAN DWIVEDI <DUBEYANSHU555@GMAIL.COM>
2022-05-10 14:49:08 +0530

    update

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,8 +3,11 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from pathlib import Path
 from django.urls import path, re_path
 from apps.home import views
+from django.views.generic import TemplateView
+from .views import *
 
 urlpatterns = [
 
@@ -13,5 +16,11 @@ urlpatterns = [
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
+    path('Review/',TemplateView.as_view(template_name='home/Review.html')),
+    path('Css/',TemplateView.as_view(template_name='home/Css.html')),
+    path('Js/',TemplateView.as_view(template_name='home/Js.html')),
+    path('Profile/',TemplateView.as_view(template_name='home/Profile.html')),
+    
+
 
 ]
