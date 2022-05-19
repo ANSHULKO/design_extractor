80178e085df6d908067161efe05acacca5fb7f13
ANSHUMAN DWIVEDI <DUBEYANSHU555@GMAIL.COM>
2022-05-19 17:07:50 +0530

    update

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(CORE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
